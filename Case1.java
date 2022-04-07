518c671a6ca823e931b3eb55c5fb56dc4497580c
rblancolll <rblancol@miumg.edu.gt>
2022-04-07 13:43:18 -0600

    Ejercicios

--- Case1.java
+++ Case1.java
@@ -1,64 +1,59 @@
 
 package case1;
+import static case1.Metodo3Y4.scanner;
 import java.util.Scanner;
 
 public class Case1 {
 static Scanner scanner=new Scanner (System.in);
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
        
         Metodo1Y2 objetoUno=new Metodo1Y2();
         
+        System.out.print("Factorial\n");     
         System.out.println("Ingrese un numero");
         int numero=scanner.nextInt();
         System.out.println("El resultado del factorial es: " + objetoUno.numeroFactorial(numero));
         
+        System.out.print("-------------------------------------\n");   
         
-        System.out.print("Ingrese un número entero: ");                                                         
-        int numero1 = scanner.nextInt();
-        System.out.println("El resulato es: "+objetoUno.tablaMultiplicar(numero1));              
-        
+        System.out.print("Tabla de Multiplicar\n");     
+        System.out.print("Ingrese un número entero:\n");                                                         
+        int numero1 =scanner.nextInt();
+        System.out.println(objetoUno.tablaMultiplicar(numero1));              
+       // Thread.sleep(3000);
                 
         
+     
         
+        Metodo3Y4 objetoDos=new Metodo3Y4();
         
-      
+        System.out.println("Digite la cantidad de numero");
+         //int cantidad1= scanner.nextInt();
+        System.out.println(objetoDos.numerosPares(10));
+       
+            // if (Terminar<=0){
+             //   romper = false;
+                //        }else{
+               //  System.out.println(" ");
+            
+           //  }
+        // break;
+        // }
+         
+         
         System.out.println("Días de la semana:");
-        int dia;
-        System.out.println("1.- Luness.");
+        System.out.print("Elige un dia de la semana");
+        System.out.println("\n1.- Lunes.");
         System.out.println("2.- Martes.");
         System.out.println("3.- Miércoles.");
         System.out.println("4.- Jueves.");
         System.out.println("5.- Viernes.");
         System.out.println("6.- Sábado.");
         System.out.println("7.- Domingo.");
-        dia = scanner.nextInt();
-    
-        switch (dia) {
-        case 1:
-        System.out.println("El día seleccionado es: ");
-        break;
-        case 2:
-        System.out.println("El día seleccionado es: DÍA LABORAL");
-        break;
-        case 3:
-        System.out.println("El día seleccionado es: DÍA LABORAL");
-        break;
-        case 4:
-        System.out.println("El día seleccionado es: DÍA LABORAlL");
-        break;
-        case 5:
-        System.out.println("El día seleccionado es: DÍA LABORAL");
-        break;
-        case 6:
-        System.out.println("El día seleccionado es: DÍA NO LABORAL");
-        break;
-        case 7:
-        System.out.println("El día seleccionado es: DÍA NO LABORAL");
-        break;
-        default:
-        System.out.println("ERROR: número incorrecto.");
-        
-     }
-  }
+        int uno= scanner.nextInt();
+        System.out.print(""+objetoDos.semana(uno));
+     
+    }
+
 }
--- Metodo3Y4.java
+++ Metodo3Y4.java
@@ -1,14 +1,83 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package case1;
 
+package case1;
+import static case1.Case1.scanner;
+import java.util.Scanner;
 
 public class Metodo3Y4 {
-     public static float diaSemana(float semana) {
+    static  Scanner scanner=new Scanner (System.in);
+   
+       public int numerosPares(int numerosPares){
+           
+         int cantPar=0, cantImpar=0, Prom, Mayor=0, Terminar, Acum=0, sumaPar=0, sumaImpar=0;
+         int cantidad1= scanner.nextInt();
+         boolean romper = true;
          
-         System.out.println("LUNES");
+         while (romper) {
+             for (int i =1; i<cantidad1; i++){
+                 System.out.println("Digite numero" +i);
+                 int num = scanner.nextInt();
+                 
+                 if(num%2==0){
+                     cantPar++;
+                     sumaPar= sumaPar+num;
+                 }else{
+                   cantImpar++;                
+                   sumaImpar= sumaImpar+num;
+                 }
+            if(num >=Mayor){
+                Mayor = num;
+            }
+            Acum= sumaImpar+sumaPar;
+            
+            }
+            Prom= cantImpar;
+            
+              System.out.println("Cantidad de numero pares es :" + cantPar);
+              System.out.println("Cantidad de numeros impares es :" + cantImpar);
+              System.out.println("La suma total es de :" + Acum);
+              System.out.println("El promedio de los numero impares :" + Prom);
+              System.out.println("0.Salir ");
+              Terminar = scanner.nextInt(); 
+              if (Terminar<=0){
+              romper = false;
+              }else{
+              System.out.println(" ");
+              }
+         }    
+        return numerosPares;
+       
+       }
+    
+        public int semana(int semana){
+       
+        switch (semana){
+        case 1:
+        System.out.println("El día seleccionado es: DÍA LABORAL");
+        break;
+        case 2:
+        System.out.println("El día seleccionado es: DÍA LABORAL");
+        break;
+        case 3:
+        System.out.println("El día seleccionado es: DÍA LABORAL");
+        break;
+        case 4:
+        System.out.println("El día seleccionado es: DÍA LABORAlL");
+        break;
+        case 5:
+        System.out.println("El día seleccionado es: DÍA LABORAL");
+        break;
+        case 6:
+        System.out.println("El día seleccionado es: DÍA NO LABORAL");
+        break;
+        case 7:
+        System.out.println("El día seleccionado es: DÍA NO LABORAL");
+        break;
+        default:
+        System.out.println("ERROR: número incorrecto.");
+            
+    }
+        return semana;
+ }
+    }
          
-     }
-}
+     
